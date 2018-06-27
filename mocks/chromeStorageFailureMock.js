e4f23184eb0a45d8280fc68f6ec4b79158743f60
Elcoosp <elcoosp@gmail.com>
2018-06-27 17:36:12 +0200

    test(mocks): Remove all values which were passed to the callback and instead delay the function setE

--- mocks/chromeStorageFailureMock.js
+++ mocks/chromeStorageFailureMock.js
@@ -1,53 +1,28 @@
-const {
-  KEY_VALUE,
-  ON_CHANGE_LISTENER_NAMESPACE,
-  BYTES_IN_USE,
-  RUNTIME_ERROR
-} = require('./mockConstants')
 const { delay } = require('../__tests__/utils')
-
+const { RUNTIME_ERROR } = require('./mockConstants')
 //Runtime global variable
 chrome = {
   runtime: {}
 }
 
+const setErrorAndCall = cb => {
+  chrome.runtime.lastError = RUNTIME_ERROR
+  cb()
+}
+
 const storageAreaFailureFactory = () => ({
-  get: (keys, cb) =>
-    delay(() => {
-      chrome.runtime.lastError = RUNTIME_ERROR
-      cb(KEY_VALUE)
-    }),
-  set: (keys, cb) =>
-    delay(() => {
-      chrome.runtime.lastError = RUNTIME_ERROR
-      cb()
-    }),
-  getBytesInUse: (keys, cb) =>
-    delay(() => {
-      chrome.runtime.lastError = RUNTIME_ERROR
-      cb(BYTES_IN_USE)
-    }),
-  remove: (keys, cb) =>
-    delay(() => {
-      chrome.runtime.lastError = RUNTIME_ERROR
-      cb()
-    }),
-  clear: cb =>
-    delay(() => {
-      chrome.runtime.lastError = RUNTIME_ERROR
-      cb()
-    })
+  get: (keys, cb) => delay(setErrorAndCall, cb),
+  set: (keys, cb) => delay(setErrorAndCall, cb),
+  getBytesInUse: (keys, cb) => delay(setErrorAndCall, cb),
+  remove: (keys, cb) => delay(setErrorAndCall, cb),
+  clear: cb => delay(setErrorAndCall, cb)
 })
 const storageAreaFailure = {
   sync: storageAreaFailureFactory(),
   local: storageAreaFailureFactory(),
   managed: storageAreaFailureFactory(),
   onChanged: {
-    addListener: cb =>
-      delay(() => {
-        chrome.runtime.lastError = RUNTIME_ERROR
-        cb(KEY_VALUE, ON_CHANGE_LISTENER_NAMESPACE)
-      })
+    addListener: cb => delay(setErrorAndCall, cb)
   }
 }
 
